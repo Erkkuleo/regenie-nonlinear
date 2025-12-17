fd99cbb6b3852e53ac21cf5b95c2b920edef367b
ergs <erkka.kiukkonen@aalto.fi>
2025-12-17 20:52:58 +0200

    added inverse functions

--- src/Nonlinear.cpp
+++ src/Nonlinear.cpp
@@ -14,23 +14,41 @@ double sinorCalculator(double value, double period, double phaseOffset) {
     return calcValue;
 }
 
+double invsinCalculator(double value, double period, double phaseOffset) {
+    double calcValue = std::asin((value / period) * 2 * M_PI + phaseOffset);
+    return calcValue;
+}
+
+double invcosCalculator(double value, double period, double phaseOffset) {
+    double calcValue = std::acos((value / period) * 2 * M_PI + phaseOffset);
+    return calcValue;
+}
+
 double calculateNonlinear(std::string function, double value, double period, double phaseOffset, bool nonlinear_in_degrees) {
     if (nonlinear_in_degrees) {
-        if (function == "sin") {
+        if (function == "sinor") {
             return (sinorCalculator(value, period, phaseOffset)*(180/(M_PI)));
-        } else if (function == "cos") {
+        } else if (function == "cosinor") {
             return (cosinorCalculator(value, period, phaseOffset)*(180/(M_PI)));
-        } else if (function == "tan") {
+        } else if (function == "invsin") {
+            return (invsinCalculator(value, period, phaseOffset)*(180/(M_PI)));
+        } else if (function == "invcos") {
+            return (invcosCalculator(value, period, phaseOffset)*(180/(M_PI)));
+        }  else if (function == "tan") {
             return (std::tan(value)*(180/(M_PI)));
         } else {
             std::cerr << "Unknown function, doing simple cos: " << function << std::endl;
             return std::cos(value);
         }
     } else {
-        if (function == "sin") {
+        if (function == "sinor") {
             return sinorCalculator(value, period, phaseOffset);
-        } else if (function == "cos") {
+        } else if (function == "cosinor") {
             return cosinorCalculator(value, period, phaseOffset);
+        } else if (function == "invsin") {
+            return (invsinCalculator(value, period, phaseOffset));
+        } else if (function == "invcos") {
+            return (invcosCalculator(value, period, phaseOffset));
         } else if (function == "tan") {
             return std::tan(value);
         } else {
